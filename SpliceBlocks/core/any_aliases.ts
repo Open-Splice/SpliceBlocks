7e8747462f5c8490de4a192a9430cd1ecd8ba4eb
Reboy2000 <reyaanshsinha4@gmail.com>
2026-01-02 00:33:39 -0500

    Adding files

---
+++ SpliceBlocks/core/any_aliases.ts
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright 2025 Sinha Group
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// eslint-disable-next-line
+type AnyDuringMigration = any;
---
+++ SpliceBlocks/typings/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright 2025 Sinha Group
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as libraryBlocks from './blocks';  // Handcrafted file.
+export * from './core/blockly';
+export {libraryBlocks};
+export const JavaScript: any;
